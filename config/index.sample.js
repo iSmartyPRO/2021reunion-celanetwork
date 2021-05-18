ddf23d3c3b9e70009ee139e5e5a36b875b12c3e0
Ilias Aidar <Ilias@ilanapro.com>
2021-05-18 07:22:07 +0000

    Added config sample

---
+++ config/index.sample.js
@@ -0,0 +1,13 @@
+module.exports = {
+    APP_PORT: 8093,
+    APP_NAME: "APPLICATION FORM FOR REUNION 2021",
+    mongoURI: "mongodb://dburi",
+    sessionSecret: "mongopass",
+    allowRegistration: false,
+    mailHost: 'smtp.host.sample',
+    mailUser: 'reunion2021@host.sample',
+    mailPass: 'mailPassword',
+    mailFrom: 'CELA Reunion 2021 <reunion2021@host.com>',
+    showAppForm: true
+
+}
